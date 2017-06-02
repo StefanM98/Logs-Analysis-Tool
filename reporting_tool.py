92b8acc72035498029bc9a2487042830d1ca5ec8
StefanM <stefan.fva@gmail.com>
2017-06-01 22:25:18 -0400

    Added example output. Revised to meet PEP8 standards.

--- reporting_tool.py
+++ reporting_tool.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import psycopg2
 
+
 def connect():
-# Establishes connection to the database
+    # Establishes connection to the database
 
     global conn
     global c
@@ -15,8 +17,9 @@ def connect():
     # Initialize cursor
     c = conn.cursor()
 
+
 def reportResults(Name, type):
-# Prints results of queries back into the console
+    # Prints results of queries back into the console
 
     result = c.fetchall()
     print "\n", Name, ":\n"
@@ -24,10 +27,12 @@ def reportResults(Name, type):
         print ' ', row[0], ' - ', row[1], type
 
 
-def articleTitleAuthor():
-# Displays all article titles and their authors
+# Attempt connection to database
+connect()
 
-    connect()
+
+def articleTitleAuthor():
+    # Displays all article titles and their authors
 
     # SQL command
     c.execute("""
@@ -39,14 +44,10 @@ def articleTitleAuthor():
     # Display Result
     reportResults("All article titles and their authors", " ")
 
-    # Disconect from database
-    conn.close()
 
-
-def topArticles(): 
-# Displays the top three articles of all time. Most popular article at the top.
-
-    connect()
+def topArticles():
+    # Displays the top three articles of all time.
+    # Most popular article at the top.
 
     # SQL command
     c.execute("""
@@ -58,58 +59,49 @@ def topArticles():
     # Display Result
     reportResults("Top 3 Articles of All Time", "views")
 
-    # Disconect from database
-    conn.close()
-
 
 def topAuthors():
-# Displays the most popular article authors of all time. Most popular author at the top.
-
-    connect()
+    # Displays the most popular article authors of all time.
+    # Most popular author at the top.
 
     # SQL command
     c.execute("""
-        select 
-            name, 
+        select
+            name,
             count(path) as num
-        from 
+        from
             authors, log, articles
-        where author = authors.id and slug = substr(path, 10) 
-        group by name 
+        where author = authors.id and slug = substr(path, 10)
+        group by name
         order by num desc limit 3;
     ;""")
 
     # Display Result
     reportResults("Top 3 Authors of All Time", "views")
 
-    # Disconect from database
-    conn.close()
-
 
 def errorCheck():
-# Displays which days had more than 1% of requests lead to errors.
-
-    connect()
+    # Displays which days had more than 1% of requests lead to errors.
 
     # SQL command
-
-
     c.execute("""
-    
+
     select
         PercentErrored.LogDate,
         to_char((round(PercentErrored.PercentageOf404, 2)), '999D99%')
-        
+
     from
     (
-        select 
-            Errored.LogDate, 
+        select
+            Errored.LogDate,
             Errored.HTTPStatus404Total,
-            ((Errored.HTTPStatus404Total / (select count(*) as total from log where time::date = Errored.LogDate)) * 100) as PercentageOf404
-            from 
+            ((Errored.HTTPStatus404Total / (select count(*) as total
+                from log
+                where time::date = Errored.LogDate)) * 100) as PercentageOf404
+            from
             (
-                select 
-                    time::date as LogDate, 
+                select
+                    time::date as LogDate,
                     count(*) * 1.0 as HTTPStatus404Total
                 from log
                 where substr(status, 1, 3) = '404'
@@ -120,12 +112,14 @@ def errorCheck():
     """)
 
     # Display Result
-    reportResults("Days where more than 1% of requests lead to errors", " ")
+    reportResults("Days where more than 1% of requests lead to errors",
+                  "errored")
 
-    # Disconect from database
-    conn.close()
 
-#articleTitleAuthor()
+articleTitleAuthor()
 topArticles()
 topAuthors()
 errorCheck()
+
+# Disconect from database
+conn.close()
